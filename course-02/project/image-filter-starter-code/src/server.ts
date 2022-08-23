319a9528c3f6e8c3e3d87e0a966432a62b56b2ee
bhoang <bhoang@invidi.com>
2022-08-24 00:52:01 +0700

    commit code for project

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -14,28 +14,35 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.use(bodyParser.json());
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
+  app.get("/", async (req: express.Request, res: express.Response) => {
+    res.send("try Get/filteredimage?image_url={{}}")
+  })
+  
+  app.get("/filteredimage", async ( req: express.Request, res: express.Response ) => {
+    const { image_url } = req.query
+    if (image_url) {
+      try {
+        const result = await filterImageFromURL(image_url)
+        res.status(200).sendFile(result, err => {
+          if (err) {
+            res.status(404).send("sent file unsuccessful!")
+          } else {
+            deleteLocalFiles([result])
+          }
+        })
+      } catch {
+        res.status(500).send("Something wrong! try again!")
+      }
+    }
+    else{
+      res.status(404).send("get image url fail!")
+    }
+  } );
 
   //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
   
 
   // Start the Server
